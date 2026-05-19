cfeab5655f0429c06a1c6efe14f0df34d618a92b
MD ZIHAD HOSSAIN <zihadhossain.dev@gmail.com>
2026-05-19 19:20:12 +0600

    Add Recommendation model import to views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
-from .models import Hero, About, Skill, Project, Experience, Education, BlogPost, Contact,
+from .models import Hero, About, Skill, Project, Experience, Education, BlogPost, Contact, Recommendation
 
 def index(request):
     if request.method == 'POST':
